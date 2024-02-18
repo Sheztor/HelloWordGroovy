6b5c98a9041c220f8cf6889777ae0612f7eb9707
Sheztor <hfsa2327@gmail.com>
2024-02-18 19:46:08 -0400

    Uso del File I/O

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,3 +1,24 @@
 static void main(String[] args) {
   println "Hello world!"
 }
+
+static  void FileIO(){
+  def archivo = new File("C:\\Users\\hfsa2\\OneDrive - INTEC\\Trimestres\\T-6\\Tendencias En Desarrollo De Software\\S-1\\GroovyProyect\\HelloWordGroovy\\src\\hola.txt")
+
+  if(archivo.exists()){
+    println "El archivo existe."
+    // archivo.delete()
+  }else{
+    archivo.createNewFile()
+    println "EL archivo ha sido creado."
+  }
+
+  archivo.write("Hola desde Groovy.")
+
+  def contenidoArchivo = archivo.text
+  println "El contenido del archivo es: $contenidoArchivo"
+}
+
+
+
+FileIO()
